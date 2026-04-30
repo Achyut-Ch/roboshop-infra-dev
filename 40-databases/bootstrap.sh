c273895ec31a519e9abef032162316caa8beb43f
Achyuth Ch <achyuth2806@gmail.com>
2026-04-30 09:06:50 -0400

    actions

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -5,8 +5,8 @@ environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
-rm -rf ansible-roboshop-roles-tf
 git clone https://github.com/Achyut-Ch/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
-ansible-playbook -e component=$component  -e environment=$environment roboshop.yaml 
+git pull
+ansible-playbook -e component=$component  -e env=$environment roboshop.yaml 
