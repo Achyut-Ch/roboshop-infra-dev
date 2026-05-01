9aa9cb1e9c30d3cffda1ae45a2cb9191588fbf96
Achyuth Ch <achyuth2806@gmail.com>
2026-04-30 22:57:40 -0400

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -120,6 +120,45 @@ resource "terraform_data" "mysql" {
   }
 }
 
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  # in which subnet we want to launch this instance, we can use subnet id 
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-rabbitmq"
+    },
+    local.common_tags
+  )
+} 
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.rabbitmq.private_ip
+  }
+  
+  provisioner "file" {
+    source = "bootstrap.sh" # local file which we want to copy on remote machine
+    destination = "/tmp/bootstrap.sh" # destination path on remote machine
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo /tmp/bootstrap.sh rabbitmq  dev"
+    ]
+  }
+}
+
 
 
 
