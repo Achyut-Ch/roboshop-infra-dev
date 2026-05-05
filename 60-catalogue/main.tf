da1e73dd94d79d1fe839ac2d3e2917a4fa97922a
Achyuth Ch <achyuth2806@gmail.com>
2026-05-05 13:28:02 -0400

    terraform

---
+++ 60-catalogue/main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "catalogue" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  # in which subnet we want to launch this instance, we can use subnet id 
+  subnet_id = local.private_subnet_ids
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+} 
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.catalogue.private_ip
+  }
+  
+  provisioner "file" {
+    source = "bootstrap.sh" # local file which we want to copy on remote machine
+    destination = "/tmp/bootstrap.sh" # destination path on remote machine
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh catalogue dev"
+    ]
+  }
+}
---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+variable "zone_id" {
+    default = "Z08611933L3R2OHMLONKD"
+}
+
+variable "domain_name" {
+    default = "daws88.sbs"
+}
